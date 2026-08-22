443cb701bca4a85d477849f6e9f74482215bccdb
Mariam Ashraf <mariamashraffffff999@gmail.com>
2026-08-22 19:17:55 +0300

    Question01

--- Assignment_9/Program.cs
+++ Assignment_9/Program.cs
@@ -1,9 +1,27 @@
 ﻿namespace Assignment_9;
 
-    internal class Program
-    {
-        static void Main(string[] args)
+  internal class Program
+  {
+     static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+        #region Assignment_9
+
+        #region Question_1
+        /* Q1: What is a generic class? Why use generics? */
+
+        // Answer ->
+        // A generic class is a class that allows us to define type parameter instead of specifying a specific data type.
+        // It allows the same class to work with different data types, where the actual data type is specified when the class is used.
+
+        // Why?
+        // 1-> Type Safety: Errors are caught at compile time instead of runtime.
+        // 2-> Code Reusability: One implementation can work with many different types.
+        // 3-> Better Performance: Avoids boxing and unboxing for value types.
+        // 4-> Cleaner Code: Reduces code duplication.
+
+         
+        #endregion
+
+        #endregion
         }
     }
